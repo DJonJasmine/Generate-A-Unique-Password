d471c047a096d38c5710a16df3e2b8716a211933
DJonJasmine <harrisondjon1994@gmail.com>
2022-11-04 22:12:07 -0700

    updated script.js and index.html file

--- assets/script.js
+++ assets/script.js
@@ -1,59 +1,64 @@
 /* 
-Step 1 create a series of prompts for password criteria 
- - choose length of password (8 - 128 characters)
- - ask do i want each character types (lowercase, uppercase, numeric, and/or special characters)
- -
- -
-step 2
+Rules 
+choose 8-128 characters
+yes/no lowercase, uppercase, numerics, special characters
+Step 1 list variables that are needed in for the password criteria 
+ - Password length (8 - 128 characters)
+ - Lowercase
+ - Numerics
+ - Special Characters
 
-WHEN I click the button to generate a password
-THEN I am presented with a series of prompts for password criteria
-WHEN prompted for password criteria
-THEN I select which criteria to include in the password
-WHEN prompted for the length of the password
-THEN I choose a length of at least 8 characters and no more than 128 characters
-WHEN asked for character types to include in the password
-THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-WHEN I answer each prompt
-THEN my input should be validated and at least one character type should be selected
-WHEN all prompts are answered
-THEN a password is generated that matches the selected criteria
-WHEN the password is generated
-THEN the password is either displayed in an alert or written to the page
-```
+step 2 - create prompt and  confirm
+
 */
 
-
 // Assignment Code
-let generateBtn = document.querySelector("#generate");
-
-
+var generateBtn = document.querySelector('#generate');
+// Password variables and values
+const lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'],
+      upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'],
+      numerics = ['0', '2', '3', '4', '5', '6', '7', '8', '9'],
+      specialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '{', '}', '~', '?', '<', '>', '/'];
+    
+      console.log(lowerCase);
+      console.log(upperCase);
+      console.log(numerics);
+      console.log(specialCharacters);
 // Write password to the #password input
 function writePassword() {
-  let password = generatePassword();
-  let passwordText = document.querySelector("#password");
+  var password = generatePassword(); {
+  var passwordText = document.querySelector('#password');
 
   passwordText.value = password;
-
 }
- 
-function generatePassword() {
-  let passwordLength = prompt("How many characters to include in your password? It may be between 8 and 128 characters.")
- 
-  let lowercase = confirm("Do you want to include lowercase characters?");
-  
-  let uppercase = confirm("Do you want to include uppercase characters?");
- 
-  let number = confirm("Do you want to include numbers?");
-  
-  let specialCharacters = confirm("Do you want to include special characters?");
- }
- 
-
- 
-
-
-generatePassword()
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener('click', writePassword); 
+
+function generatePassword() {
+
+  // User prompt to choose password length
+  const passwordLength = prompt("How many password characters would you like to include? Password may be between 8 to 128 characters.");
+  // User confirm for lowercase characters in password 
+  const lowerCase = confirm("Do you want to include lowercase characters?");
+  // User confirm for uppercase characters in password 
+  const upperCase = confirm("Do you want to include uppercase characters?");
+  // User confirm for numbers in password
+  const numerics = confirm("Do you want to include numbers?");
+  // User confirm for special characters in password
+  const specialCharacters = confirm("Do you want to include special characters?");
+
+  console.log(passwordLength);
+  console.log(lowerCase);
+  console.log(upperCase);
+  console.log(numerics);
+  console.log(specialCharacters);
+
+
+
+
+ }
+ 
+}
+writePassword();
+generatePassword();
